f6038c1978a9db397f53af533337df5efeb6dd12
hossein zare <hosseiny625@gmail.com>
2024-06-05 00:29:14 +0330

    feat: add semantic-release-pub with new config

--- release.config.js
+++ release.config.js
@@ -3,6 +3,13 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    [
+     "semantic-release-pub",
+     {
+      "cli": "flutter",
+      "updateBuildNumber" : true
+     }
+    ],
     ["@semantic-release/git", {
       "assets": ["dist/*.js", "dist/*.js.map"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
