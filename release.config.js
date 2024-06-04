ff18da50c85a64c8cdc52876d9e9210d168b8e51
hossein zare <hosseiny625@gmail.com>
2024-06-05 01:38:18 +0330

    feat: read release version and put it in commit message

--- release.config.js
+++ release.config.js
@@ -16,7 +16,13 @@ const config = {
       "assets": ["dist/*.js", "dist/*.js.map"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    '@semantic-release/github'
+    '@semantic-release/github',
+    [
+          '@semantic-release/exec',
+          {
+            verifyReleaseCmd: 'echo ${nextRelease.version} > release_version.txt'
+          }
+    ]
   ]
 };
 
